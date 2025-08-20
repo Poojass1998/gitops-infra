f21a59a3a003f358f71a1b7175f8e35dfdb87ff4
Poojass1998 <121761524+Poojass1998@users.noreply.github.com>
2025-08-20 10:13:53 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "gitops-rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "gitops-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "gitopsdemo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "gitopsacr12345"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
